965b65f4fa3d2a1c73a9258963d76fc33a76fa36
Remix Workspace <>
2022-10-28 13:19:22 +0900

    udemy till 51

--- contracts/OnlyOwnerMint.sol
+++ contracts/OnlyOwnerMint.sol
@@ -2,11 +2,11 @@
 
 pragma solidity ^0.8.14;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.6.0/token/ERC721/ERC721.sol";
 
-contract MyNFT is ERC721
+contract OnlyOwnerMint is ERC721
 {
-    constructor() ERC721("MyNFT","MYNFT"){}
+    constructor() ERC721("OnlyOwnerMint","OWNER"){}
     function nfgMint(address to, uint256 tokenId) public {
         _mint(to,tokenId);
     }
